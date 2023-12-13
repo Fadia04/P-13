54476e06db7216deeedb95594dc9dd0db1e9d311
Fadia04 <fl.medja@gmail.com>
2023-12-13 11:49:47 +0100

    New tests for the views

--- services/services/tests/unit_tests/test_views.py
+++ services/services/tests/unit_tests/test_views.py
@@ -1,6 +1,6 @@
 import pytest
 from django.urls import reverse
-from pytest_django.asserts import assertTemplateUsed
+from pytest_django.asserts import assertTemplateUsed, assertRedirects, assertContains
 from django.test import Client
 from offers.models import Offer, Category, Comment
 from users.models import User
@@ -21,9 +21,20 @@ def test_login_page_view():
     client = Client()
     User.objects.create(username="test_user", password="123456789")
     path = reverse("signin")
-    response = client.post(path, {"username": "test_user", "password": "123456789"})
+    response = client.get(path, {"username": "test_user", "password": "123456789"})
     assert response.status_code == 200
     assertTemplateUsed(response, "users/signin.html")
+    
+@pytest.mark.django_db
+def test_login_page_success_redirect_to_home_view():
+    client = Client()
+    User.objects.create(username="test_user", password="123456789")
+    
+    path = reverse("signin")
+    response = client.post(path, {"username": "test_user", "password": "123456789"})
+    assertRedirects(
+        response=response, expected_url="/", status_code=302, target_status_code=200
+    )
     
 @pytest.mark.django_db
 def test_logout_page_view():
@@ -34,7 +45,7 @@ def test_logout_page_view():
     assert response.url == "/signin/"
     
 @pytest.mark.django_db
-def test_signup_page_view():
+def test_signup_page_success_redirect_to_home_view():
     client = Client()
     test_user = {
         "first_name": "Loulou",
@@ -47,141 +58,173 @@ def test_signup_page_view():
     response = client.post(path, test_user)
     assert response.status_code == 302
     #assertTemplateUsed(response, "users/signup.html")
-    assert response.url == "/"
+    #assert response.url == "/"
+    assertRedirects(
+        response=response, expected_url="/", status_code=302, target_status_code=200
+    )
     
 @pytest.mark.django_db
 def test_add_comment_view():
     client = Client()
     test_user=User.objects.create(username="test_user", password="5678")
     cat = Category.objects.create(name="bricolage")
-    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="demande")
+    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="Offre")
     User.objects.create(username="Lamia", password="lili5678")
     client.login(username="Lamia", password="lili5678")
     path = reverse("add-comment", kwargs={"id": offer.id})
     #data = {"comment_body": "CommentTextMessageTest", "username": "Lamia" , "password": "lili5678" }
-    response = client.post(path)
+    response = client.get(path)
     assert response.status_code == 302
-    #assertTemplateUsed(response,"offers/add_comment.html")
+    #path = reverse("view_offer", kwargs={"id": offer.id})
+    assertTemplateUsed(response, "offers/view_offer.html")
     
+
 @pytest.mark.django_db
 def test_create_offer_view():
     client = Client()
+    User.objects.create(username="Lamia", password="lili5678")
+    client.login(username="Lamia", password="lili5678")
     
     path = reverse("create-offer")
-    response = client.post(path)
+    response = client.get(path)
     
 
-    assert response.status_code == 302
-    assertTemplateUsed(response,"offers/offers.html")
+    assert response.status_code == 200
+    assertTemplateUsed(response,"offers/create_offer.html")
     
 @pytest.mark.django_db
-def view_offer_view():
+def test_view_offer_view():
     client = Client()
     test_user=User.objects.create(username="test_user", password="5678")
     cat = Category.objects.create(name="bricolage")
-    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="offre")
+    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="Offre")
     User.objects.create(username="Lamia", password="lili5678")
     client.login(username="Lamia", password="lili5678")
-    path = reverse("view_offer", kwargs={"id": offer.id})
+    path = reverse("view_offer", kwargs={"offer_id": offer.id})
     #data = {"comment_body": "CommentTextMessageTest", "username": "Lamia" , "password": "lili5678" }
     response = client.get(path)
     content = response.content.decode()
-    expected_content = "perceuse échange perceuse bricolage offre"
-    assert content == expected_content
+    #expected_content = "perceuse échange perceuse bricolage"
+    #assert content == expected_content
     assert response.status_code == 200
+    assertContains(response, "perceuse")
     assertTemplateUsed(response,"offers/view_offer.html")
     
 @pytest.mark.django_db
-def view_request_view():
+def test_modifie_offer_view():
     client = Client()
     test_user=User.objects.create(username="test_user", password="5678")
     cat = Category.objects.create(name="bricolage")
-    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="demande")
+    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="Offre")
     User.objects.create(username="Lamia", password="lili5678")
     client.login(username="Lamia", password="lili5678")
-    path = reverse("view_request", kwargs={"id": offer.id})
+    path = reverse("modifie_offer", kwargs={"offer_id": offer.id})
+    response = client.post(path)
+    content = response.content.decode()
+    #expected_content = "perceuse échange perceuse bricolage"
+    #assert content == expected_content
+    assert response.status_code == 200
+    assertContains(response, "perceuse")
+    assertTemplateUsed(response,"offers/modifie_offer.html")
+    
+    
+@pytest.mark.django_db
+def test_view_request_view():
+    client = Client()
+    test_user=User.objects.create(username="test_user", password="5678")
+    cat = Category.objects.create(name="bricolage")
+    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="Demande")
+    User.objects.create(username="Lamia", password="lili5678")
+    client.login(username="Lamia", password="lili5678")
+    path = reverse("view_offer", kwargs={"offer_id": offer.id})
     #data = {"comment_body": "CommentTextMessageTest", "username": "Lamia" , "password": "lili5678" }
     response = client.get(path)
     content = response.content.decode()
-    expected_content = "perceuse échange perceuse bricolage demande"
-    assert content == expected_content
-    assert response.status_code == 200
-    assertTemplateUsed(response,"offers/view_request.html")
+    #expected_content = "échange perceuse bricolage demande"
+    #assert content == expected_content
+    assertContains(response, "perceuse")
+    assert response.status_code == 200    
+    assertTemplateUsed(response,"offers/view_offer.html")
     
 @pytest.mark.django_db
-def view_requests_view():
+def test_view_requests_view():
     client = Client()
     test_user=User.objects.create(username="test_user", password="5678")
     cat = Category.objects.create(name="bricolage")
-    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="demande")
-    User.objects.create(username="Lamia", password="lili5678")
-    client.login(username="Lamia", password="lili5678")
-    path = reverse("view_offers")
+    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="Demande")
+    #User.objects.create(username="Lamia", password="lili5678")
+    client.login(username="test_user", password="5678")
+    path = reverse("view_requests")
     response = client.get(path)
     content = response.content.decode()
-    expected_content = "perceuse bricolage"
-    assert content == expected_content
+    #expected_content = "perceuse bricolage"
+    #assert content == expected_content
+    assert "perceuse" in content
     assert response.status_code == 200
-    assertTemplateUsed(response,"offers/view_offers.html")
+    assertTemplateUsed(response,"offers/view_requests.html")
     
 @pytest.mark.django_db
-def view_offers_view():
+def test_view_offers_view():
     client = Client()
     test_user=User.objects.create(username="test_user", password="5678")
     cat = Category.objects.create(name="bricolage")
-    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="offre")
-    #User.objects.create(username="test_user", password="5678")
+    offer = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat, user=test_user, type="Offre")
+    offer2= Offer.objects.create(title="maths", description = "donne cours de maths", category=cat, user=test_user, type="Offre")
     client.login(username="test_user", password="5678")
     path = reverse("view_offers")
     response = client.get(path)
     content = response.content.decode()
-    expected_content = "perceuse bricolage échange"
-    assert content == expected_content
+    #expected_content = "perceuse bricolage échange"
+    #assert content == expected_content
     assert "perceuse" in content
+    assert "maths" in content
     assert response.status_code == 200
     assertTemplateUsed(response,"offers/view_offers.html")
+    #assertContains(response, "perceuse")
     
 @pytest.mark.django_db
-def view_categories_view():
+def test_view_categories_view():
     client = Client()
-    categories = Category.objects.create(name1="bricolage", name2= "pet_sitting")
+    categories = Category.objects.create(name="Bricolage")
     #category2=
     path = reverse("view_categories")
     response = client.get(path)
     content = response.content.decode()
-    expected_content = "bricolage pet_sitting"
-    assert content == expected_content
+    #expected_content = "bricolage"
+    #assert content == expected_content
     assert response.status_code == 200
     assertTemplateUsed(response,"offers/view_categories.html")
-    
+    assert "Bricolage" in content
     
 @pytest.mark.django_db
-def view_category_view():
+def test_view_category_view():
     client = Client()
-    category = Category.objects.create(name="bricolage")
-    path = reverse("view_category", kwargs={category.id: "id"})
+    category = Category.objects.create(name="Bricolage")
+    path = reverse("view_category", kwargs={category.id: "category_id"})
     response = client.get(path)
     content = response.content.decode()
-    expected_content = "bricolage"
+    expected_content = "Bricolage"
     assert content == expected_content
     assert response.status_code == 200
     assertTemplateUsed(response,"offers/view_category.html")
+    assert "Bricolage" in content
     
 @pytest.mark.django_db
-def search_view():
+def test_search_view():
     client = Client()
     test_user=User.objects.create(username="test_user", password="5678")
     cat1 = Category.objects.create(name="bricolage")
     cat2 = Category.objects.create(name="cours")
-    offer1 = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat1, user=test_user, type="offre")
-    offer2 = Offer.objects.create(title="marteau", description = "prète un marteau", category=cat2, user=test_user, type="offre")
-    User.objects.create(username="Lamia", password="lili5678")
-    client.login(username="Lamia", password="lili5678")
+    offer1 = Offer.objects.create(title="perceuse", description = "échange une perceuse", category=cat1, user=test_user, type="Offre")
+    offer2 = Offer.objects.create(title="maths", description = "donne cours de maths", category=cat2, user=test_user, type="Offre")
+    #User.objects.create(username="Lamia", password="lili5678")
+    #client.login(username="Lamia", password="lili5678")
     path = reverse("search")
-    response = client.post(path, {"query": "perceuse"})
+    response = client.post(path, {"query": "maths"})
     content = response.content.decode()
-    expected_content = "perceuse échange une perceuse bricolage offre"
+    expected_content = "maths cours"
     assert content == expected_content
     assert response.status_code == 200
     assertTemplateUsed(response,"offers/search.html")
-    
+    #assert response.context["offers"][0]==offer2
+    #assert response.context["offers"][1]==offer1
