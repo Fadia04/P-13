cc874695caa73d26859e5e81d0a0b4deefa468bd
Fadia04 <fl.medja@gmail.com>
2024-02-05 00:53:07 +0100

    Modified settings again

--- services/services/settings.py
+++ services/services/settings.py
@@ -182,11 +182,3 @@ LOGIN_REDIRECT_URL = "/signin/"
 # fotoblog/settings.py
 AUTH_USER_MODEL = 'users.User'
 
-# Following settings only make sense on production and may break development environments.
-if not DEBUG:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
